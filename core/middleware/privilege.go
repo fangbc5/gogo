db9ba65d3fe2288c4ff6567fcc8389bef59b30d7
fangbchun <fangbchun@yonyou.com>
2023-04-21 15:10:23 +0800

    认证jwt方式1

--- core/middleware/privilege.go
+++ core/middleware/privilege.go
@@ -1,10 +1,12 @@
 package middleware
 
 import (
+	"errors"
 	"github.com/fangbc5/gogo/constant"
 	"github.com/fangbc5/gogo/core/auth"
+	"github.com/fangbc5/gogo/core/common"
+	"github.com/fangbc5/gogo/plugins/auth/jwt"
 	"github.com/fangbc5/gogo/utils"
-	"log"
 	"net/http"
 	"regexp"
 	"strings"
@@ -12,7 +14,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Privilege() gin.HandlerFunc {
+func Privilege(publicKey string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		path := c.Request.URL.Path
 		//method := c.Request.Method
@@ -36,58 +38,30 @@ func Privilege() gin.HandlerFunc {
 			c.Next()
 		} else if utils.IsBlack(token) {
 			//如果token为空则用户未登录直接返回
-			c.JSON(http.StatusOK, model.GetRspAll(http.StatusUnauthorized, "未登录，请登录后访问", nil))
+			c.JSON(http.StatusUnauthorized, common.GetRsp(http.StatusUnauthorized, "未登录，请登录后访问", nil))
 			c.Abort()
 		} else {
-			//不在白名单中，判断用户是否已登录
-			username := db.RedisCache("get", constant.TokenKey+token)
-			if utils.IsNull(username) {
-				//用户登录状态已过期或token无效
-				c.JSON(http.StatusOK, common.GetRspAll(http.StatusUnauthorized, "用户登录状态已过期或token错误，请重新登录", nil))
+			//校验token
+			jwtImpl := jwt.NewAuth(auth.WithPublicKey(publicKey))
+			if _, err := jwtImpl.Inspect(token); err != nil {
+				c.JSON(http.StatusUnauthorized, common.GetRsp(http.StatusUnauthorized, "token无效", nil))
 				c.Abort()
-				return
-			} else {
-				//casbinValid(c,username,path,method)
-				ok, msg := tokenValid(token)
-				if ok {
-					c.Next()
-				} else {
-					c.Abort()
-					c.JSON(http.StatusOK, common.GetRspAll(http.StatusUnauthorized, msg, nil))
-					return
-				}
 			}
+			c.Next()
 		}
 	}
 }
 
-func casbinValid(sub string, obj string, act string) (bool, string) {
+func casbinValid(sub string, obj string, act string) error {
 	//用户已登录,使用casbin进行鉴权操作
 	e := auth.MakeEnforcer()
 	e.EnableLog(true)
-	err := e.LoadPolicy()
-	if err != nil {
-		log.Panicln("Casbin LoadPolicy Error")
+	if err := e.LoadPolicy(); err != nil {
+		return errors.New("casbin LoadPolicy Error")
 	}
 	//权限认证
-	ok, err := e.Enforce(sub, obj, act)
-	if err != nil {
-		return false, "Casbin Enforce Error"
+	if ok, err := e.Enforce(sub, obj, act); !ok || err != nil {
+		return errors.New("casbin Enforce Error")
 	}
-	if ok {
-		return true, "验证成功"
-	} else {
-		return false, "验证失败"
-	}
-}
-
-func tokenValid(token string) (bool, string) {
-	claims, err := auth.VerifyToken(token)
-	if err != nil {
-		return false, err.Error()
-	}
-	if claims == nil {
-		return false, "claims is nil"
-	}
-	return true, "校验成功"
+	return nil
 }
